2a913bff9a3d67021c641e631a6eb22927cc1ba8
aluckman3142 <adamluckman@adams-mbp.home>
2024-06-30 13:25:04 +0100

    Display task list

--- app/Livewire/Showtasks.php
+++ app/Livewire/Showtasks.php
@@ -3,9 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Task;
 
 class Showtasks extends Component
 {
+    public function mount() {
+        $this->tasks = Task::all();
+        
+    }
+
     public function render()
     {
         return view('livewire.showtasks');
--- resources/views/livewire/showtasks.blade.php
+++ resources/views/livewire/showtasks.blade.php
@@ -1,3 +1,51 @@
 <div>
-    <h2 class="text-2xl text-red-500 font-bold underline">Task List</h2>
+
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Task Title
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Priority
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   Due Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($this->tasks as $task)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$task->title}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$task->priority}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$task->due_date}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$task->status}}
+                </td>
+                <td class="px-6 py-4">
+                   Mark Completed
+                </td>
+
+            </tr>
+            @endforeach
+            
+        </tbody>
+    </table>
+</div>
+
 </div>
